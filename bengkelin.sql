586fedb7a2a76b4c7d5793684f7c81f9ed77432f
mohammn <mvapdg@gmail.com>
2022-04-12 09:48:06 +0700

    upload sql

---
+++ bengkelin.sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 12, 2022 at 04:45 AM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 7.3.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bengkelin`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `antrian`
+--
+-- Error reading structure for table bengkelin.antrian: #1932 - Table 'bengkelin.antrian' doesn't exist in engine
+-- Error reading data for table bengkelin.antrian: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `bengkelin`.`antrian`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bayar`
+--
+-- Error reading structure for table bengkelin.bayar: #1932 - Table 'bengkelin.bayar' doesn't exist in engine
+-- Error reading data for table bengkelin.bayar: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `bengkelin`.`bayar`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jasa`
+--
+-- Error reading structure for table bengkelin.jasa: #1932 - Table 'bengkelin.jasa' doesn't exist in engine
+-- Error reading data for table bengkelin.jasa: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `bengkelin`.`jasa`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+-- Error reading structure for table bengkelin.user: #1932 - Table 'bengkelin.user' doesn't exist in engine
+-- Error reading data for table bengkelin.user: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `bengkelin`.`user`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
